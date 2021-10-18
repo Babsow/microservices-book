4dda640e9d8589a6b478325343caf675a38ba12c
babasow <babasow120@gmail.com>
2021-10-18 17:44:21 +0200

    initialisation

---
+++ src/main/java/microservices/book/multipliaction/SocialMultipliactionApplication.java
@@ -0,0 +1,13 @@
+package microservices.book.multipliaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMultipliactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMultipliactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microservices/book/multipliaction/SocialMultipliactionApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.book.multipliaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMultipliactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
