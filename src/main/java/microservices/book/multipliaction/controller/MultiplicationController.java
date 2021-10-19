b21b51993cd15fa4520a48da8bee9c480b98be70
babasow <babasow120@gmail.com>
2021-10-20 01:06:37 +0200

    Multiplication Controller V2

--- src/main/java/microservices/book/multipliaction/controller/MultiplicationController.java
+++ src/main/java/microservices/book/multipliaction/controller/MultiplicationController.java
@@ -2,18 +2,27 @@ package microservices.book.multipliaction.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import microservices.book.multipliaction.domain.Multiplication;
 import microservices.book.multipliaction.service.MultiplicationService;
 
 
 @RestController
+@RequestMapping("/multiplications")
 public class MultiplicationController {
     private final MultiplicationService multipliactionService ;
 
     @Autowired
     public MultiplicationController(final MultiplicationService multiplicationService){
        this.multipliactionService = multiplicationService;
+  }
+
+  @GetMapping("/random")
+  Multiplication getRandomMultiplication(){
+    return multipliactionService.createRandomMultiplication() ;
   }
 
 }
