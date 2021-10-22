86ec0cb4d8200f8a3c2431532144dd5c2a4c4eed
babasow <babasow120@gmail.com>
2021-10-22 10:16:28 +0200

    JQuery updated

--- src/main/resources/static/multiplication-client.js
+++ src/main/resources/static/multiplication-client.js
@@ -1,9 +1,9 @@
 function updateMultiplication() {
   $.ajax({ url: "http://localhost:8080/multiplications/random"}).then(function(data) {
   // Cleans the form
-  $("#attempt-form").find( "input[name='result-attempt']" ).val("");
+  $("#attempt-form").find( "input[name='result-attempt']" ).val("44444444444");
   $("#attempt-form").find( "input[name='user-alias']" ).val("");
-  // Gets a random challenge from API and loads the datain the HTML
+  // Gets a random challenge from API and loads the data in the HTML
   $('.multiplication-a').empty().append(data.factorA);
   $('.multiplication-b').empty().append(data.factorB);
   });
@@ -18,10 +18,8 @@ function updateMultiplication() {
   var b = $('.multiplication-b').text();
   var $form = $( this ),
   attempt = $form.find( "input[name='result-attempt']" ).val(),
-  userAlias = $form.find( "input[name='user-alias']" )
-.val();
-// Compose the data in the format that the API is
-expecting
+  userAlias = $form.find( "input[name='user-alias']" ).val();
+// Compose the data in the format that the API is expecting
 var data = { user: { alias: userAlias}, multiplication:
 {factorA: a, factorB: b}, resultAttempt: attempt};
 // Send the data using post
@@ -33,7 +31,7 @@ contentType: "application/json; charset=utf-8",
 dataType: "json",
 success: function(result){
 if(result.correct) {
-$('.result-message').empty().append("Theresult is correct! Congratulations!");
+$('.result-message').empty().append("The result is correct! Congratulations!");
 } else {
 $('.result-message').empty().append("Oops that's not correct! But keep trying!");
 }
