bf53e36de873ce995340420dcccdae149143cf07
babasow <babasow120@gmail.com>
2021-10-25 23:51:50 +0200

    User data Persistence

--- src/main/java/microservices/book/multipliaction/domain/User.java
+++ src/main/java/microservices/book/multipliaction/domain/User.java
@@ -1,5 +1,11 @@
 package microservices.book.multipliaction.domain;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +15,13 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class User {
   
+  @Id
+  @GeneratedValue
+  @Column(name = "USER_ID")
+  private Long id;
   private final String alias;
 
   protected User(){
