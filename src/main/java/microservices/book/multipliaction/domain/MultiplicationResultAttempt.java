8435b4b51e25fd7fa1523ee9aa2bde751f28e06b
babasow <babasow120@gmail.com>
2021-10-25 23:59:03 +0200

    mltAttemp data Persistenc

--- src/main/java/microservices/book/multipliaction/domain/MultiplicationResultAttempt.java
+++ src/main/java/microservices/book/multipliaction/domain/MultiplicationResultAttempt.java
@@ -2,6 +2,13 @@ package microservices.book.multipliaction.domain;
 
 
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +18,19 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class MultiplicationResultAttempt {
   
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @ManyToOne(cascade = CascadeType.PERSIST )
+  @JoinColumn(name = "USER_ID")
   private final User user;
+
+  @ManyToOne(cascade = CascadeType.PERSIST)
+  @JoinColumn(name = "MULTIPLICATION_ID")
   private final Multiplication multiplication;
   private final int resultAttempt ; 
   private final boolean correct ;
