864ac9635171ba178bd300490566fbeff6998358
babasow <babasow120@gmail.com>
2021-10-25 23:28:40 +0200

    Multiplication Persistance done

--- src/main/java/microservices/book/multipliaction/domain/Multiplication.java
+++ src/main/java/microservices/book/multipliaction/domain/Multiplication.java
@@ -1,4 +1,9 @@
 package microservices.book.multipliaction.domain;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +14,13 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class Multiplication {
-  
+
+  @Id
+  @GeneratedValue
+  @Column(name = "MULTIPLICATION_ID")
+  private Long id;
   private final int factorA;
   private final int factorB;
   
